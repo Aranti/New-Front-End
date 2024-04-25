58d3c8c001820af7613abb23a91576793bc67b48
Arvind Dhupkar <35193272+arvind2904@users.noreply.github.com>
2024-04-25 17:41:57 +0530

    Update baseUrl.js

--- src/shared/baseUrl.js
+++ src/shared/baseUrl.js
@@ -1,11 +1,11 @@
 // LOCALHOST
-export const baseUrl = process.env.REACT_APP_BASE_URL;
+// export const baseUrl = process.env.REACT_APP_BASE_URL;
 
 // ACTUAL
 // export const baseUrl = process.env.REACT_APP_BASE_URL_SHIP_LINK;
 
-// TESTING
-// export const baseUrl = process.env.REACT_APP_BASE_URL_EC2_TEST;
+// TESTING 
+export const baseUrl = process.env.REACT_APP_BASE_URL_EC2_TEST;
 
 // BETA TEST
 // export const baseUrl = process.env.REACT_APP_BASE_URL_BETA_TEST;
